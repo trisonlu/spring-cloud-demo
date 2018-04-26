cf16f91d19b7c500947981386255495c40ed8b08
卢舒勤 <lushuqin443@pingan.com.cn>
2018-04-26 21:24:48 +0800

    add hystrix

--- spring-cloud-eureka/eureka-producer-consumer/eureka-consumer/src/main/java/com/lsq/spring/demo/eureka/consumer/remote/DemoRemote.java
+++ spring-cloud-eureka/eureka-producer-consumer/eureka-consumer/src/main/java/com/lsq/spring/demo/eureka/consumer/remote/DemoRemote.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by LUSHUQIN443 on 2018/4/26.
  */
-@FeignClient(name = "spring-cloud-producer")
+@FeignClient(name = "spring-cloud-producer", fallback = DemoRemoteHystrix.class)
 public interface DemoRemote {
     @RequestMapping(value = "/demo")
     public String demo(@RequestParam(value = "message") String message);
